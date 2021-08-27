306c3388d6b58b4a1b71ea7e335487f0e41ae142
Aqueuse <llo@DESKTOP-I516SA9.localdomain>
2021-08-27 16:28:08 +0200

    Here be dragons !

---
+++ Scripts/Neko.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TwitchChatConnect.Client;
+using TwitchChatConnect.Config;
+using TwitchChatConnect.Data;
+using TwitchChatConnect.Manager;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Neko : MonoBehaviour {
+
+    //OAuth = https://twitchapps.com/tmi
+void Start()
+{
+TwitchChatClient.instance.Init(() =>
+{
+TwitchChatClient.instance.onChatMessageReceived += ShowMessage;
+TwitchChatClient.instance.onChatCommandReceived += ShowCommand;
+TwitchChatClient.instance.onChatRewardReceived += ShowReward;
+}, message =>
+{
+Debug.LogError(message);
+});
+}
+void ShowCommand(TwitchChatCommand chatCommand)
+{
+    if(chatCommand.Command == "Nom de la command qui trigger "){
+
+    }
+}
+void ShowReward(TwitchChatReward chatReward)
+{
+
+}
+void ShowMessage(TwitchChatMessage chatMessage)
+{
+}
+}
